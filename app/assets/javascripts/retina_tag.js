efe2e4e7ea8c3f6ebaadd5cd5b602533887b99f9
David Estes <destes@redwindsw.com>
2012-12-14 10:11:01 -0500

    refactored retina tag js to be live and support lazy modee

--- app/assets/javascripts/retina_tag.js
+++ app/assets/javascripts/retina_tag.js
@@ -1,17 +1,46 @@
-function highdpi_init() {
-  if(window.devicePixelRatio > 1)
-  {
-    var els = jQuery("img").get();
-    for(var i = 0; i < els.length; i++) {
-      var hiDpiSrc = $(els[i]).attr('hidpi_src');
+var RetinaTag = RetinaTag || {};
 
-      if(hiDpiSrc) {
-        els[i].src = hiDpiSrc;
-      }
+RetinaTag.init = function() {
+  RetinaTag.updateImages();
+  RetinaTag.interval = setInterval(RetinaTag.updateImages,50);
+};
+
+RetinaTag.updateImages = function() {
+  var images = document.getElementsByTagName('img');
+  for(var counter=0; counter < images.length; counter++) {
+    RetinaTag.refreshImage(images[counter]);
+  }
+};
+
+RetinaTag.refreshImage = function(image) {
+  var lazyLoad  = image.getAttribute('data-lazy-load');
+  var imageSrc  = image.src;
+  var hiDpiSrc  = image.getAttribute('hidpi_src');
+  var lowDpiSrc = image.getAttribute('lowdpi_src');
+  if(!hiDpiSrc) {
+    return;
+  }
+
+  if(window.devicePixelRatio > 1 && imageSrc != hiDpiSrc) {
+    if(!lowDpiSrc) {
+      image.setAttribute('lowdpi_src',imageSrc);
+    }
+    if(lazyLoad) {
+      lazyLoad.setAttribute('data-lazy-load','2x');
+    }
+    else {
+      image.src = hiDpiSrc;
+    }
+
+  }
+  else if(window.devicePixelRatio <= 1 && imageSrc == hiDpiSrc) {
+    if(lazyLoad) {
+      lazyLoad.setAttribute('data-lazy-load','1x');
+    }
+    else {
+      image.src = lowDpiSrc;
     }
   }
-}
+};
 
-$(document).ready(function() {
-  highdpi_init();
-});
+RetinaTag.init();
